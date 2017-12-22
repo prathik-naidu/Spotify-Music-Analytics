705f60bdfd8a8854604a8ccf566b2c4e4c81289d
Prathik Naidu <prathik213@gmail.com>
2017-12-21 23:37:51 -0500

    Finished Audio Feature Extraction

--- get_data.py
+++ get_data.py
@@ -8,8 +8,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-#Get Playlist
-playlists = sp.user_playlists('12165262567')
+#Get Playlist Code
+#playlists = sp.user_playlists('12165262567')
 '''while playlists:
     for i, playlist in enumerate(playlists['items']):
         print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
@@ -24,16 +24,15 @@ songs = []
 items = []
 ids = []
 
-content = sp.user_playlist_tracks('12165262567', '11Fo4ON0gWVQ56aPXgjOk3', fields=None, limit=100, market=None)
-print(content)
-songs += content['items']
-#if content['next'] is not None:
-#    offset += 100
-#else:
-#    break
+#Get Audio Features for a Playlist
+#content = sp.user_playlist_tracks('12165262567', '11Fo4ON0gWVQ56aPXgjOk3', fields=None, limit=100, market=None)
+results = sp.user_playlist_tracks('12165262567', '11Fo4ON0gWVQ56aPXgjOk3')
+songs = results['items']
+while results['next']:
+    results = sp.next(results)
+    songs.extend(results['items'])
 
-#print("HELLO")
-#print(songs)
+
 for i in songs:
     ids.append(i['track']['id'])
 
@@ -42,8 +41,6 @@ audio_features = []
 while index < len(ids):
     audio_features += sp.audio_features(ids[index:index + 50])
     index += 50
-
-
 
 features_list = []
 for features in audio_features:
@@ -65,28 +62,31 @@ df = pd.DataFrame(features_list, columns=['energy', 'liveness',
 
 #print(df)
 
+writer = pd.ExcelWriter('/Users/Prathik/Desktop/output.xlsx')
+df.to_excel(writer, 'Sheet1')
 
-tid = 'spotify:track:4TTV7EcfroSLWzXRY6gLv6'
 
-tids = ['spotify:track:4TTV7EcfroSLWzXRY6gLv6','spotify:track:3JIxjvbbDrA9ztYlNcp3yL']
 
-'''start = time.time()
-analysis = sp.audio_analysis(tid)
-delta = time.time() - start
-print(json.dumps(analysis, indent=4))
-print ("analysis retrieved in %.2f seconds" % (delta,))'''
+def main(username, playlist):
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    print "Getting user playlist"
+    get_user_playlist(username, sp)
+    print "Getting playlist content"
+    get_playlist_content(username, playlist, sp)
+    print "Getting playlist audio features"
+    get_playlist_audio_features(username, playlist, sp)
 
-start = time.time()
-features = sp.audio_features(tid)
 
-#print(features)
-#print(features['energy'])
+if __name__ == '__main__':
+    print 'Starting...'
+    parser = argparse.ArgumentParser(description='description')
+    parser.add_argument('--username', help='username')
+    parser.add_argument('--playlist', help='username')
+    args = parser.parse_args()
+    main(args.username, args.playlist)
 
-delta = time.time() - start
-'''for feature in features:
-    print(json.dumps(feature, indent=4))
-    print()
-    analysis = sp._get(feature['analysis_url'])
-    print(json.dumps(analysis, indent=4))
-    print()'''
-print ("features retrieved in %.2f seconds" % (delta,))
+
+
+
+
+
